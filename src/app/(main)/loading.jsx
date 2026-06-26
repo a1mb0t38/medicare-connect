76bcdd3ad70ed17581c11c3da817a05f554a68ee
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-26 22:07:52 +0600

    added not found and loading page

---
+++ src/app/(main)/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mainloading = () => {
+    return (
+        <div>
+          <div className='flex h-[85vh] items-center justify-center'>
+            <span className="loading loading-bars loading-xl text-blue-800"></span>
+        </div>  
+        </div>
+    );
+};
+
+export default Mainloading;
