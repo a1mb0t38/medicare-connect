9660b1f0296cb7356ea241be5b2a5ce3c5a273f7
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-29 20:29:45 +0600

    added the jwttoken verfication to /doctors

--- src/lib/action/doctors.js
+++ src/lib/action/doctors.js
@@ -1,14 +1,23 @@
 
 'use server'
 
+import { auth } from "../auth"
+
 const baseUrl = process.env.BASE_URL
 
 export const getDoctors = async (name='', specialization='') =>{
+      const  {token} = await auth.api.getToken({
+            headers: await headers()
+        })
     const params = new URLSearchParams()
     if(name) params.set('name', name)
     if(specialization) params.set('specialization', specialization)
     const qs = params.toString()
     const url = qs ? `${baseUrl}/doctors?${qs}` : `${baseUrl}/doctors`;
-    const res = await fetch(url, {cache: 'no-store'})
+    const res = await fetch(url,{
+        headers: {
+                    authorization: `Bearer ${token}`
+                }
+    }, {cache: 'no-store'})
     return await res.json()
 }
