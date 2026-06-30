39714e62d7615a4d7a6c3195cca097eabd352e6d
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-30 20:14:51 +0600

    removed console log

--- src/app/(main)/dashboard/prescriptions/page.jsx
+++ src/app/(main)/dashboard/prescriptions/page.jsx
@@ -3,27 +3,27 @@ import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
 import React from 'react';
 
-const PrescriptionPage = async() => {
+const PrescriptionPage = async () => {
 
     const session = await auth.api.getSession({
         headers: await headers(),
     })
     const user = session?.user;
 
-    const  {token} = await auth.api.getToken({
+    const { token } = await auth.api.getToken({
         headers: await headers()
     })
 
-    const res = await fetch(`${process.env.BASE_URL}/appointments/doctor/${user.id}/accepted`,{
-         headers: {
-                            authorization: `Bearer ${token}`
-                        }
-    },{
+    const res = await fetch(`${process.env.BASE_URL}/appointments/doctor/${user.id}/accepted`, {
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    }, {
         cache: 'no-store',
     })
     const appointments = await res.json()
 
-    console.log(appointments)
+    
 
     return (
         <div>
