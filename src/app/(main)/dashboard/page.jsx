8cc830f361f5fcce8295872523d5634349988a76
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-06-30 10:32:56 +0600

    added some note for new registered doctor

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -15,7 +15,9 @@ const DashBoardPage = async() => {
 
     return (
         <div>
-            
+            <div className='min-h-screen flex items-center justify-center'>
+              <h2>Note for New registered doctor: in order to get yourself listed on find doctor section for patient, you need to save your info. go to doctor info in sidebar and there You have to put your info and save it. then patient will find you and make appointment for you.</h2>
+            </div>
         </div>
     );
 };
